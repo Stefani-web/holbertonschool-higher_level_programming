c88fab6b983ff5efef8a3f44e0e5e0dbc43973d9
Stefani-web <stephaniecvlho@gmail.com>
2024-05-06 16:14:02 +0200

    Change Code Test

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-# Loop over ASCII values for lowercase letters (97 to 122) and print each character
-for i in range(97, 123):
-    print("{:s}".format(chr(i)), end="") # Convert ASCII value to character and print without newline
+for i in range(97, 123): # Loop over ASCII values for lowercase letters (97 to 122) and print each character
+    print("{:s}".format(chr(i)), end="")  # Convert ASCII value to character and print without newline
