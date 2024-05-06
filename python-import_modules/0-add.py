83e0faea9545ae72209c5117e744b37f7d344b25
Stefani-web <stephaniecvlho@gmail.com>
2024-05-07 00:12:19 +0200

    Write a program that imports the function def add(a, b) File

---
+++ python-import_modules/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# Import the add function from add_0.py
+from add_0 import add
+
+# Assign the values 1 and 2 to variables a and b
+a = 1
+b = 2
+
+# Call the add function with a and b as arguments and print the result
+result = add(a, b)
+print("{} + {} = {}".format(a, b, result))
