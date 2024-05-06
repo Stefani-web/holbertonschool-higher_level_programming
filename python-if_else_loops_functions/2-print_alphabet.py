aa2aecc05849f08342b3f7fb76dac148be4fa145
Stefani-web <stephaniecvlho@gmail.com>
2024-05-06 15:53:43 +0200

    Adding Write a program that prints the ASCII alphabet, in lowercase File

---
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+# Loop over ASCII values for lowercase letters (97 to 122) and print each character
+for i in range(97, 123):
+    print(chr(i), end='')  # Convert ASCII value to character and print without newline
