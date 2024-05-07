9e5e363eb6d3798f5069d4887df45e26920978ff
Stefani-web <stephaniecvlho@gmail.com>
2024-05-07 15:13:48 +0200

    Add if __name__ == "__main__"

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-import sys
+if __name__ == "__main__":
+    import sys
 
-# Initialize the sum
-total_sum = 0
+    # Initialize the sum
+    total_sum = 0
 
-# Iterate over arguments and sum up their integer values
-for arg in sys.argv[1:]:
-    total_sum += int(arg)
+    # Iterate over arguments and sum up their integer values
+    for arg in sys.argv[1:]:
+        total_sum += int(arg)
 
-# Print the total sum
-print(total_sum)
+    # Print the total sum
+    print(total_sum)
