d45a882089ec9ffa730fe6af0459b1299ce95bbb
Stefani-web <stephaniecvlho@gmail.com>
2024-05-07 16:39:31 +0200

    Write a program that prints all the names defined by the compiled module hidden_4.pyc

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
+    import hidden_4
 
-    # Initialize the sum
-    total_sum = 0
-
-    # Iterate over arguments and sum up their integer values
-    for arg in sys.argv[1:]:
-        total_sum += int(arg)
-
-    # Print the total sum
-    print(total_sum)
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
