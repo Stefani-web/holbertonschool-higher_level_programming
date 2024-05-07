aac7115f82e745f2554d41b73809ffafe410ffeb
Stefani-web <stephaniecvlho@gmail.com>
2024-05-07 15:11:48 +0200

    Add first loop If

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-import sys
+if __name__ == "__main__":
+    import sys
 
-# Get the number of arguments
-num_args = len(sys.argv) - 1  # Subtract 1 to exclude the script name
+    # Get the number of arguments
+    num_args = len(sys.argv) - 1 # Subtract 1 to exclude the script name
 
-# Print the number of arguments
-if num_args == 0:
-    print("Number of argument(s): 0.")
-    print(".")
-else:
-    print("Number of argument(s):", num_args, "argument" if num_args == 1 else "arguments", ":")
+    # Print the number of arguments
+    if num_args == 0:
+        print("Number of argument(s): 0.")
+        print(".")
+    else:
+        print("Number of argument(s):", num_args, "argument" if num_args == 1 else "arguments", ":")
 
     # Print each argument
     for i in range(num_args):
