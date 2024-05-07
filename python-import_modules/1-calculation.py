a1cdf42bf0a363b3fc1da9d6eee425cf798a99e7
Stefani-web <stephaniecvlho@gmail.com>
2024-05-07 15:07:21 +0200

    Write a program that imports functions from the file calculator_1.py, does some Maths, and prints the result

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-# Import specific functions from calculator_1.py
-from calculator_1 import add, subtract, multiply, divide
+if __name__ == '__main__':
+    a = 10
+    b = 5
 
-# Define the variables a and b
-a = 10
-b = 5
-
-# Call the imported functions with a and b as arguments and print the results
-print("Addition:", add(a, b))
-print("Subtraction:", subtract(a, b))
-print("Multiplication:", multiply(a, b))
-print("Division:", divide(a, b))
+        # Import specific functions from calculator_1.py
+    from calculator_1 import add, sub, mul, div
+        # Call the imported functions with a and b as arguments and print the results
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
