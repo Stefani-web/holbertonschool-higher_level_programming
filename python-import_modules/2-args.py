20d2b42f5c5df605680b2c3b196a85af2fb81acd
Stefani-web <stephaniecvlho@gmail.com>
2024-05-08 11:57:04 +0200

    Fix errors

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys import argv
+    import sys
+    index = len(sys.argv) - 1
 
-    # Get the number of arguments
-    num_args = len(argv)
-
-    # Print the number of arguments
-    if num_args - 1 == 0:
-        print("{} arguments.\n".format(num_args - 1), end="")
-        print("{} argument:\n".format(num_args - 1), end="")
+    if index == 0:
+        print("{} arguments.".format(index))
+    elif index == 1:
+        print("{} argument:".format(index))
     else:
-        print("{} arguments:\n".format(num_args - 1), end="")
+        print("{} arguments:".format(index))
 
-    # Print each argument
-    if num_args != 0:
-        for index in range(1, num_args):
-            print("{}: {}".format(index, argv[index]))
+    if index >= 1:
+        index = 0
+        for arg in sys.argv:
+            if index != 0:
+                print("{}: {}".format(index, arg))
+            index += 1
