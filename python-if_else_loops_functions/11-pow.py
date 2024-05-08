46d436399323ab9ec045719b7a09272654b5b13c
Stefani-web <stephaniecvlho@gmail.com>
2024-05-08 11:37:46 +0200

    Fix errors

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 def pow(a, b):
+    if b == 0:
+        return 1
+    elif b < 0:
+        return 1 / pow(a, -b)
     result = 1
-    for i in range(b):
-        result *= a
+    while b > 0:
+        if b % 2 == 1:  # If b is odd
+            result *= a
+        a *= a
+        b //= 2
     return result
