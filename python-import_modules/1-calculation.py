7e5c338ad1fe481a4644f2dce45e8000f1b11a55
Stefani-web <stephaniecvlho@gmail.com>
2024-05-08 10:19:23 +0200

    change list import

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-if __name__ == '__main__':
-    a = 10
-    b = 5
-
-        # Import specific functions from calculator_1.py
+if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
-        # Call the imported functions with a and b as arguments and print the results
-    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
-    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
-    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+
+a = 10
+b = 5
+
+print("{} + {} = {}".format(a, b, add(a, b)))
+print("{} - {} = {}".format(a, b, sub(a, b)))
+print("{} * {} = {}".format(a, b, mul(a, b)))
+print("{} / {} = {}".format(a, b, div(a, b)))
