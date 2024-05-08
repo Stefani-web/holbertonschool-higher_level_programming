2fc7d6fb242d0c815942eda1c3cd61fb7e4ee0bf
Stefani-web <stephaniecvlho@gmail.com>
2024-05-08 10:38:03 +0200

    Change loop For

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -2,7 +2,6 @@
 if __name__ == "__main__":
     import hidden_4
 
-    names = dir(hidden_4)
-    for name in names:
-        if name[:2] != "__":
-            print(name)
+    for index in sorted(dir(hidden_4)):
+        if index[:2] != "__":
+            print(index)
