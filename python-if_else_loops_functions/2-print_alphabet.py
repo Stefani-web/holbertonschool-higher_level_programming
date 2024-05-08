a5ef6b1fd7a2c273832b52dcfcbd4a98cbfbe1c6
Stefani-web <stephaniecvlho@gmail.com>
2024-05-08 11:05:16 +0200

    Fix errors

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for i in range(97, 123): # Loop over ASCII values for lowercase letters (97 to 122) and print each character
-    print("{:s}".format(chr(i)), end="")   # Convert ASCII value to character and print without newline
+for index in range(97, 123):
+    print("{:s}".format(chr(index)), end="")
