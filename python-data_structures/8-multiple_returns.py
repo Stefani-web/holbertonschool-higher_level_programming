9cbca464f3446aa1d36850e5b0f59afa2ede235c
Stefani-web <stephaniecvlho@gmail.com>
2024-05-13 16:50:57 +0200

    Fix pycodestyle

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -4,7 +4,6 @@ def multiple_returns(sentence):
     # If the sentence is empty, return (0, None)
     if len(sentence) == 0:
         return (0, None)
-    
     else:
         # Return a tuple with the length of the string and its first character
         return (len(sentence), sentence[0])
