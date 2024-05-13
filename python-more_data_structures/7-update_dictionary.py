ed2de6d2c6b8668cbaa286d47244b1b5aac5ef24
Stefani-web <stephaniecvlho@gmail.com>
2024-05-13 16:18:35 +0200

    Add a function that replaces or adds key/value in a dictionary

---
+++ python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+def update_dictionary(a_dictionary, key, value):
+    # Update value of the key if it exists, otherwise add the key-value pair
+    a_dictionary[key] = value
+    return a_dictionary
