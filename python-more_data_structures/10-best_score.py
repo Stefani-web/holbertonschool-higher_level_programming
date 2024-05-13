3ce57979aafefc6d4d58fad84b9a010f45eee81e
Stefani-web <stephaniecvlho@gmail.com>
2024-05-13 22:09:53 +0200

    Correct the syntax

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,14 +1,15 @@
-
+#!/usr/bin/python3
 
 def best_score(a_dictionary):
-    # Initialize variables to store the maximum score and corresponding key
-    max_score = None
-    max_key = None
+    if a_dictionary is None:
+        return None
 
-    # Iterate through the dictionary to find the key with the maximum value
+    best_key = None
+    best_value = 0
+
     for key, value in a_dictionary.items():
-        if max_score is None or value > max_score:
-            max_score = value
-            max_key = key
+        if value > best_value:
+            best_key = key
+            best_value = value
 
-    return max_key
+    return best_key
