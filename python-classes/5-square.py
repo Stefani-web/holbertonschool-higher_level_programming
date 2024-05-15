6b341c3350400405393a876568ebb0d26798d1c7
Stefani-web <stephaniecvlho@gmail.com>
2024-05-15 13:43:31 +0200

    Correct for checker

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -1,25 +1,13 @@
 #!/usr/bin/python3
 """
-A class used to represent a Square.
-
-    Attributes
-
-    size (int): The size of one side of the square.
-
-    Methods
-
-    area(): Returns the area of the square.
-    my_print(): Prints the square with the character '#'.
+Represents a square
 """
 
 
 class Square:
+    """A geometric square with a size attribute"""
     def __init__(self, size=0):
-        """Initialize the square with a given size.
-
-        Parameters
-        size : (int, optional) size of one side of the square (default is 0)
-        """
+        """Initialize the square with a given size."""
         self.size = size
 
     @property
@@ -36,7 +24,7 @@ class Square:
     def size(self, value):
         """Set the size of the square.
         Param.:
-            value [int] : The size of one side of the square.
+            value (int) : The size of one side of the square.
 
         Raises:
             TypeError: If value is not an integer.
@@ -51,18 +39,14 @@ class Square:
     def area(self):
         """Calculate the area of the square.
 
-        Returns
-
+        Returns:
         int
             The area of the square.
         """
         return self.__size ** 2
 
     def my_print(self):
-        """Print the square with the character '#'.
-
-        If the size of the square is 0, prints an empty line.
-        """
+        """Print the square with the character '#'."""
         if self.__size == 0:
             print()
         else:
