a430f030dfa244fcc4dcd3b550e29dbc19b9f83c
Stefani-web <stephaniecvlho@gmail.com>
2024-05-15 10:23:00 +0200

    Fix pycodestyle in documentation comments

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
-"""a Square class that defines a square
+"""
+a Square class that defines a square
 """
 
 
 class Square:
-    """Square class that define a square with is self size
+    """
+    Square class that define a square with is self size
     """
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        """instance attribute
+        """
+        instance attribute
         """
         self.__size = size
