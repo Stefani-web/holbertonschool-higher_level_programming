4e05fc30a85552c1d0788bb85f687d8a2e50451d
Stefani-web <stephaniecvlho@gmail.com>
2024-05-21 14:54:36 +0200

    Correct errors fix docstring

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python3
-"""A function add_integer that adds 2 integers"""
+"""
+
+A function add_integer that adds 2 integers
+
+"""
 
 
 def add_integer(a, b=98):
-    """Returns: the sum of a and b"""
-
+    """
+    Adds two integers together.
+    """
     if not isinstance(a, (int, float)):
-        raise TypeError("a must be an integer or a float")
+        raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
-        raise TypeError("b must be an integer or a float")
-
-    return int(a) + int(b)
+        raise TypeError("b must be an integer")
+    a = int(a)
+    b = int(b)
+    return a + b
