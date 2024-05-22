9be68c55894b759ab5b0dafd01009b6b404c9095
Stefani-web <stephaniecvlho@gmail.com>
2024-05-22 16:06:14 +0200

    Correct code execution

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -4,8 +4,12 @@
 
 def inherits_from(obj, a_class):
     """
-    Returns True if the object is an instance of a class that inherited
-    (directly or indirectly) from the specified class;
-    otherwise False.
+    Returns a list of strings indicating whether the object is an instance
+    of a class that inherited (directly or indirectly)
+    from the specified class
     """
-    return issubclass(type(obj), a_class)
+    result = []
+    for cls in type(obj).__mro__[1:]:
+        if issubclass(cls, a_class):
+            result.append(f"True inherited from class {cls.__name__}")
+    return result
