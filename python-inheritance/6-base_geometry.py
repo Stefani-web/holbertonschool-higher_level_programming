c56e1897fba735368db168d15ac910520cf30100
Stefani-web <stephaniecvlho@gmail.com>
2024-05-22 16:31:07 +0200

    Add a class BaseGeometry (based on 5-base_geometry.py)

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module contains the "BaseGeometry" class"""
+
+
+class BaseGeometry:
+    """A "BaseGeometry" class with a public instance method "area" """
+
+    def area(self):
+        """Throws an exception with the message "area() is not implemented" """
+        raise Exception("area() is not implemented")
