9316be5cc020cdd3939fc18d9cb2a60fc922ad2f
Stefani-web <stephaniecvlho@gmail.com>
2024-05-23 10:00:18 +0200

    Correct code

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -1,30 +1,41 @@
 #!/usr/bin/python3
-"""This module contains the Rectangle class"""
+""" Rectangle class """
 
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+"""
+This module defines the Rectangle class which inherits from BaseGeometry.
+It is used to represent a rectangle with a specific width and height.
+"""
 
 
 class Rectangle(BaseGeometry):
-     """This class represents a rectangle."""
-def __init__(self, width, height):
-        """
-         Initializes a new instance of the Rectangle class.
+    """A class used to represent a Rectangle, inheriting from BaseGeometry
+    Attributes:
+    __width (int): The width of the rectangle, a private instance attribute
+    __height (int): The height of the rectangle, a private instance attribute
+    """
 
-         Args:
-             width (int): The width of the rectangle.
-             height (int): The height of the rectangle.
-         """
+    def __init__(self, width, height):
+        """Initialize a new Rectangle instance
+        Args:
+            width (int): The width of the rectangle
+            height (int): The height of the rectangle
+        """
         self.__width = width
         self.__height = height
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
+        self.integer_validator("width", self.__width)
+        self.integer_validator("height", self.__height)
 
-
-def area(self):
-        """ Returns the area of the rectangle"""
+    def area(self):
+        """
+        Public instance method: def area(self):
+        that returns the current rectangle area
+        """
         return self.__width * self.__height
 
-
-def __str__(self):
-        """Returns a string representation of the rectangle"""
+    def __str__(self):
+        """
+        Return the string representation of the rectangle.
+        """
         return "[Rectangle] {}/{}".format(self.__width, self.__height)
