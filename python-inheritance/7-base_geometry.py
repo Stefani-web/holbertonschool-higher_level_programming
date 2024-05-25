a76d6e70738631aa939b69fb360ff7c7cc378675
Stefani-web <stephaniecvlho@gmail.com>
2024-05-25 11:21:36 +0200

    Correct output

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,16 +1,29 @@
 #!/usr/bin/python3
-"""This module defines the BaseGeometry class with specific methods"""
+"""7-base_geometry.py This module initializes a class called BaseGeometry"""
 
 
 class BaseGeometry:
-    """Represents a basic geometric shape."""
+    """The 'BaseGeometry' class"""
+
+    def __init__(self):
+        """Initialize the class"""
+        pass
+
     def area(self):
-        """Raises an Exception with message 'area() is not implemented'."""
+        """
+        Instance method: def area(self):
+        Raises an Exception with the message 'area() is not implemented'.
+        """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Validates value."""
-        if type(value) != int:
+        """
+        Instance method that validates value:
+        Raise a TypeError if value is not an int
+        Raise a ValueError if value <= 0
+        """
+        if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
+
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
