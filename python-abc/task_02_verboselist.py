06f2f062495f00e33c89cbd614170d9507d0062a
Stefani-web <stephaniecvlho@gmail.com>
2024-05-25 15:58:01 +0200

    Improvement syntax

--- python-abc/task_02_verboselist.py
+++ python-abc/task_02_verboselist.py
@@ -1,24 +1,24 @@
 class VerboseList(list):
     def append(self, item):
         super().append(item)
-        print(f"Added [{item}] to the list.")
+        print(f'Added [{item}] to the list.')
 
     def extend(self, iterable):
         super().extend(iterable)
         num_items = len(iterable)
-        print(f"Extended the list with [{num_items}] items.")
+        print(f'Extended the list with [{num_items}] items.')
 
     def remove(self, item):
         try:
             super().remove(item)
-            print(f"Removed [{item}] from the list.")
+            print(f'Removed [{item}] from the list.')
         except ValueError:
-            print(f"{item} not found in the list. Cannot remove.")
+            print(f'{item} not found in the list. Cannot remove.')
 
     def pop(self, index=None):
         if index is None:
             item = super().pop()
         else:
             item = super().pop(index)
-        print(f"Popped [{item}] from the list.")
+        print(f'Popped [{item}] from the list.')
         return item
