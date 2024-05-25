75585fd736bda01e4f5debce098ee750bdf05fab
Stefani-web <stephaniecvlho@gmail.com>
2024-05-25 11:25:22 +0200

    Correct errors syntaxe

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,30 +1,31 @@
 #!/usr/bin/python3
-"""This module contains the Square class"""
+"""A Square class """
 
-Rectangle = __import__('9-rectangle').Rectangle
+Rectangle = __import__("9-rectangle").Rectangle
+
+"""This module defines the Square class which inherits from Rectangle,
+it self which inherits from BaseGeometry.
+"""
 
 
 class Square(Rectangle):
-    """This class represents a square"""
+    """
+    A class used to represent a Square, inheriting from Rectangle.
+    Attributes:
+    __size (int): The size of the square, a private instance attribute.
+    """
 
     def __init__(self, size):
-        """
-        Initializes a new instance of the Square class.
-
+        """Initialize a new Square instance.
         Args:
             size (int): The size of the square.
         """
+        super().integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
 
     def area(self):
+        """Instance method: def area(self):
+        that returns the current rectangle area.
         """
-        Returns the area of the square.
-        """
-        return self.__size ** 2
-
-    def __str__(self):
-        """
-        Retourne description du carré sous la forme [Rectangle] <size>/<size>.
-        """
-        return "[Rectangle] {}/{}".format(self.__size, self.__size)
+        return super().area()
