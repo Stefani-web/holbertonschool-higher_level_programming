7eedbd74c5f671803c6b7f31d4381c664de8d19e
Stefani-web <stephaniecvlho@gmail.com>
2024-05-25 15:52:06 +0200

    Correct good checker

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -1,41 +1,57 @@
-#!/usr/bin/env python3
-import math
+#!/usr/bin/python3
 from abc import ABC, abstractmethod
+from math import pi
+'''Import abstractmethod'''
 
 
 class Shape(ABC):
+    '''Shape class'''
     @abstractmethod
     def area(self):
+        '''Area method'''
         pass
 
     @abstractmethod
     def perimeter(self):
+        '''Perimeter method'''
         pass
 
 
 class Circle(Shape):
+    '''Circle class'''
+
     def __init__(self, radius):
-        self.radius = radius
+        self.radius = abs(radius)
 
     def area(self):
-        return math.pi * self.radius**2
+        '''Area method circle'''
+        return pi * (self.radius ** 2)
 
     def perimeter(self):
-        return 2 * math.pi * self.radius
+        '''Perimeter method circle'''
+        return 2 * pi * self.radius
 
 
 class Rectangle(Shape):
+    '''Rectangle class'''
+
     def __init__(self, width, height):
         self.width = width
         self.height = height
 
     def area(self):
+        '''Area method rectangle'''
         return self.width * self.height
 
     def perimeter(self):
-        return 2 * (self.width + self.height)
+        '''Perimeter method rectange'''
+        return (self.width * 2 + self.height * 2)
 
 
-def shape_info(shape):
-    print("Area: {}".format(shape.area()))
-    print("Perimeter: {}".format(shape.perimeter()))
+def shape_info(self):
+    '''Calls area and perimeter methods'''
+    area = self.area()
+    perimeter = self.perimeter()
+
+    print(f'Area: {area}')
+    print(f'Perimeter: {perimeter}')
