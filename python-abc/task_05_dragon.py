1a41935011c4f8a3465ce0cced343edbd6765dea
Stefani-web <stephaniecvlho@gmail.com>
2024-05-25 15:59:44 +0200

    Improvement syntaxe

--- python-abc/task_05_dragon.py
+++ python-abc/task_05_dragon.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 class SwimMixin:
     def swim(self):
-        print("The creature swims!")
+        print('The creature swims!')
 
 
 class FlyMixin:
     def fly(self):
-        print("The creature flies!")
+        print('The creature flies!')
 
 
 class Dragon(SwimMixin, FlyMixin):
     def roar(self):
-        print("The dragon roars!")
+        print('The dragon roars!')
