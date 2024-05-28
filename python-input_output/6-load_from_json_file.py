073a4e5f5f08a31f6958768af24f69edd8354321
Stefani-web <stephaniecvlho@gmail.com>
2024-05-28 23:37:34 +0200

    Correct

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -6,7 +6,9 @@ import json
 
 
 def load_from_json_file(filename):
+
     ''' Create a Python object from a JSON file. '''
+
     with open(filename) as file:
         jsonObj = json.load(file)
     return jsonObj
