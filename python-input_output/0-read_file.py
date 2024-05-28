003baaddb866be11f5293dbb2fd5be013b36617d
Stefani-web <stephaniecvlho@gmail.com>
2024-05-28 17:56:23 +0200

    Correct errors

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -3,10 +3,10 @@
 
 
 def read_file(filename=""):
+
     '''Reads a UTF-8 encoded text file and prints its contents to stdout.
-    Args:
-    filename (str, optional): The name of the file to read.
-    If not provided, the function will attempt to read from stdin.
-    '''
+    Args: filename (str, optional): The name of the file to read.
+    If not provided, the function will attempt to read from stdin.'''
+
     with open(filename, "r", encoding="utf-8") as f:
         print(f.read())
