c931bf647853eb7b9fb74f0c3d83c0808548154a
Stefani-web <stephaniecvlho@gmail.com>
2024-06-19 23:51:14 +0200

    Update bis

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -10,8 +10,7 @@
 -- can use only one SELECT statement
 -- the database name will be passed as an argument of the mysql command
 
-  SELECT cities.id, cities.name, states.name
-    FROM cities
-    JOIN states
-   WHERE cities.state_id = states.id
+SELECT cities.id, cities.name, states.name FROM cities
+JOIN states
+WHERE cities.state_id = states.id
 ORDER BY cities.id ASCii;
