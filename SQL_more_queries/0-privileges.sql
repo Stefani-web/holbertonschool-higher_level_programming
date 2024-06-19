ba4b11057ab61e612b679d49699b843827ac808c
Stefani-web <stephaniecvlho@gmail.com>
2024-06-19 22:44:35 +0200

    Write a script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost)

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,5 +1,3 @@
--- List of privileges for user_0d_1
+-- Lists all privileges of the users user_0d_1 and user_0d_2.
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
-
--- List of privileges for user_0d_2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
