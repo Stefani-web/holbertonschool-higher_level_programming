72e2f2e0f681ee8e397774db5f1024a91f94fe57
Stefani-web <stephaniecvlho@gmail.com>
2024-06-21 10:55:20 +0200

    Update task 16

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -3,8 +3,8 @@
 -- Left join with the tv_genres table to get the genre names
 -- Sorting the results by show title and genre name in ascending order
 
-SELECT tv_shows.title, COALESCE(tv_genres.name, 'NULL') AS genre_name
+SELECT tv_shows.title, tv_genres.name
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-ORDER BY tv_shows.title, genre_name; ASC;
+ORDER BY tv_shows.title, genre_name ASC;
